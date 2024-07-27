b21235563a473e77c98f94c73bcc44234687d69a
Sebastian Pech <spech@spech.de>
2024-07-27 20:27:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using TimelineGenerator.Commands;
 var app = new CommandApp();
 app.Configure(config =>
 {
-    config.AddCommand<SampleCommand>("generate").WithDescription("Generate a timeline using an exporter.");
+    config.AddCommand<GenerateCommand>("generate").WithDescription("Generate a timeline using an exporter.");
     config.AddCommand<SampleCommand>("sample").WithDescription("Generate a sample file.");
 });
 return app.Run(args);
