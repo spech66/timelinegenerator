aaf193d02cc6343028913ab16921d9acaf2159d7
Sebastian Pech <spech@spech.de>
2024-07-27 21:58:36 +0200

    Implement mermaid exporter

--- Exporter/MermaidExporter.cs
+++ Exporter/MermaidExporter.cs
@@ -6,11 +6,52 @@ using System.Threading.Tasks;
 
 namespace TimelineGenerator.Exporter
 {
+    // Docs: https://mermaid.js.org/syntax/timeline.html
     internal class MermaidExporter : Exporter
     {
         public void Export(YamlTimeline timeline, string outputPath)
         {
-            throw new NotImplementedException();
+            var outputFile = outputPath + ".md";
+
+            using (var writer = new StreamWriter(outputFile))
+            {
+                writer.WriteLine($"# {timeline.Title}");
+                writer.WriteLine();
+                writer.WriteLine("```mermaid");
+                writer.WriteLine("timeline");
+                writer.WriteLine($"    title {timeline.Title}");
+
+                // Only date and title are supported. Categories are used as sections if multiple are present.
+                var sections = timeline.Events.Select(e => e.Category).Where(s => s != string.Empty).Distinct().Count();
+                if (sections > 1)
+                {
+                    foreach (var category in timeline.Events.Select(e => e.Category).Distinct())
+                    {
+                        writer.WriteLine($"    section {category}");
+                        foreach (var @event in timeline.Events.Where(e => e.Category == category).OrderBy(s => s.Start))
+                        {
+                            var end = @event.End.HasValue ? @event.End.Value.ToString("yyyy-MM-dd") : string.Empty;
+                            writer.WriteLine($"        {@event.Start:yyyy-MM-dd} {end} : {Escape(@event.Title)}");
+                        }
+                    }
+                } else
+                {
+                    foreach (var @event in timeline.Events.OrderBy(s => s.Start))
+                    {
+                        var end = @event.End.HasValue ? @event.End.Value.ToString("yyyy-MM-dd") : string.Empty;
+                        writer.WriteLine($"        {@event.Start:yyyy-MM-dd} {end} : {Escape(@event.Title)}");
+                    }
+                }
+
+                writer.WriteLine("```");
+                writer.WriteLine();
+                writer.WriteLine("Generated using [TimelineGenerator](https://github.com/spech66/timelinegenerator).");
+            }
+        }
+
+        private object Escape(string title)
+        {
+            return title.Replace(":", "\\:");
         }
     }
 }
